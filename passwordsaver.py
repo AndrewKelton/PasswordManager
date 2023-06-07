7c729b95cc126cf39f2dc39667807b4d9f45f906
Andrew Kelton <109199143+AndrewKelton@users.noreply.github.com>
2023-06-07 02:17:15 -0400

    Add files via upload

---
+++ passwordsaver.py
@@ -0,0 +1,23 @@
+from passwordgenerator import PasswordGen
+from passwordgenerator import password
+
+PasswordGen()
+
+f2 = open('numberOfPasswords.txt', 'r')
+p = f2.read()
+p = int(p)
+
+answer = input('Would you like to save this password? Y/N...: ')
+if answer.upper() == 'Y':
+    p+=1
+    f = open('passwords.txt', 'a')
+    f.write(f'\nPassword {p}: {password}')
+    f.close()
+    f2 = open('numberOfPasswords.txt', 'a')
+    f2.truncate(0)
+    f2.write(str(p))
+    f2.close()
+       
+else:
+    f = open('passwords.txt', 'r')
+    print(f.read())
